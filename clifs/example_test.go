bcaf54c38602ef9438eb2ae031da7f16f42d7a81
Daved <daved@codemodus.com>
2018-12-19 12:46:48 -0800

    Add clifs example.

---
+++ clifs/example_test.go
@@ -0,0 +1,42 @@
+package clifs_test
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/codemodus/clip/clifs"
+)
+
+func Example() {
+	// emulate cli command 'myapp -example=test -h'
+	os.Args = []string{"myapp", "-example=test", "-h"}
+	main()
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	var (
+		program = path.Base(os.Args[0])
+		verbose bool
+		example string
+	)
+
+	fs := flag.NewFlagSet("global", clifs.FlagErrorHandling)
+	fs.BoolVar(&verbose, "v", verbose, "verbosity")
+	fs.StringVar(&example, "example", example, "example")
+
+	if err := clifs.Parse(fs, os.Args[1:]); err != nil {
+		return clifs.Usage(program, fs, "", err)
+	}
+
+	fmt.Println(verbose, example)
+	return nil
+}
